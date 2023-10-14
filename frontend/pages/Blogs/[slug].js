2504f296e4f598496b2779ebbf48b31c8152709e
Harshit Mishra <harshitlove28@outlook.com>
2023-10-14 23:01:05 +0530

    WYsWYG type editor implemented

--- frontend/pages/Blogs/[slug].js
+++ frontend/pages/Blogs/[slug].js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 // import edjsHTML from 'editorjs-html';
 import EditorJSHTML from 'editorjs-html';
-
+import Markdown from 'react-markdown'
 const Blog = ({ blog }) => {
   console.log("Blog is", blog);
   const commentsData = blog.attributes.comments.data;
@@ -19,16 +19,20 @@ const Blog = ({ blog }) => {
           <h1 className="text-2xl text-center underline underline-offset-4">
             {blog.attributes.title}
           </h1>
+          <div className='flex '> 
+
           {/* <p>{blog.attributes.content}</p> */}
           <div dangerouslySetInnerHTML={{ __html: blog.attributes.content }} className='w-4/5 mx-auto py-10 ck-content '></div>
-
-          <div className='my-24'>
+         
+          </div>
+           
+          <div className='my-24 w-4/5 mx-auto'>
             <h1 className='text-xl font-semibold text-start px-5 py-3'>Comments</h1>
             <hr />
             {commentsData.map((comment) => (
                <div className='flex flex-col justify-center items-start px-4 py-2 rounded-lg bg-gray-300 my-4'>
                 <p className='text-xl font-bold'>{comment.attributes.name}</p>
-                <div dangerouslySetInnerHTML={convertJSONToHTML(comment.attributes.description)}></div>
+                <Markdown>{comment.attributes.description}</Markdown>
               </div>
             ))}
           </div>
